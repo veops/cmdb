5b4f95a50e47ed5e6acab6307f9915334904866c
pycook <pythoncook@163.com>
2019-08-28 20:51:51 +0800

    add ui

---
+++ ui/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view v-if="alive" />
+    </div>
+  </a-locale-provider>
+</template>
+
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import { AppDeviceEnquire } from '@/utils/mixin'
+
+export default {
+  mixins: [AppDeviceEnquire],
+  provide () {
+    return {
+      reload: this.reload
+    }
+  },
+  data () {
+    return {
+      locale: zhCN,
+      alive: true
+    }
+  },
+  mounted () {},
+  methods: {
+    reload () {
+      this.alive = false
+      this.$nextTick(() => {
+        this.alive = true
+      })
+    }
+  }
+}
+</script>
+<style>
+#app {
+  height: 100%;
+}
+</style>
---
+++ ui/src/components/tools/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logo">
+    <router-link :to="{name:'cmdb'}">
+      <h1 v-if="showTitle">{{ title }}</h1>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import LogoSvg from '@/assets/logo.svg?inline'
+
+export default {
+  name: 'Logo',
+  components: {
+    LogoSvg
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'CMDB',
+      required: false
+    },
+    showTitle: {
+      type: Boolean,
+      default: true,
+      required: false
+    }
+  }
+}
+</script>
