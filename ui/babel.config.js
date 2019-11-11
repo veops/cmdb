9798e99eb7c54859edb6b550ff868a6a483d3308
pycook <pycook@126.com>
2019-11-11 13:15:21 +0800

    Remove package-lock.json and remove some compile warnings

--- ui/babel.config.js
+++ ui/babel.config.js
@@ -2,9 +2,14 @@ module.exports = {
   presets: [
     '@vue/app',
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
-        'useBuiltIns': 'entry'
+        "useBuiltIns": "usage", // "usage" | "entry" | false, defaults to false.
+        "corejs": "3.1.2",
+        "targets": {
+          "esmodules": true,
+          "ie": "11"
+        }
       }
     ]
   ]
