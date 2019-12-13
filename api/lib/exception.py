ccf1d1c09a28b83bdefce7d527bdf6a861cea1d0
pycook <pycook@126.com>
2019-12-13 09:59:38 +0800

    catch abort exception when getting relation views

--- api/lib/exception.py
+++ api/lib/exception.py
@@ -1,5 +1,11 @@
 # -*- coding:utf-8 -*-
 
 
+from werkzeug.exceptions import NotFound, Forbidden, BadRequest
+
+
 class CommitException(Exception):
     pass
+
+
+AbortException = (NotFound, Forbidden, BadRequest)
