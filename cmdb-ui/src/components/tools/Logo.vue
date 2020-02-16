61e6f36e0cc7f31db05b22ed38f095dc873599ad
shaohaojiecoder <445923944@qq.com>
2020-02-16 18:18:15 +0800

    add a log pic

--- cmdb-ui/src/components/tools/Logo.vue
+++ cmdb-ui/src/components/tools/Logo.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="logo">
     <router-link :to="{name:'cmdb'}">
-      <h1 v-if="showTitle">{{ title }}</h1>
+      <div style="text-align: center; margin-right: 24px">
+        <div style="display: inline-block;">
+          <img src="@/assets/cmdb.png" width="50px" height="50px" style="margin:0; padding:0" />
+        </div>
+        <h1 v-if="showTitle">{{ title }}</h1>
+      </div>
     </router-link>
   </div>
 </template>
