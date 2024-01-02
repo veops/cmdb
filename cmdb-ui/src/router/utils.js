bff45d00b696cee97ed2f2f3ffaf2147a2322434
wang-liang0615 <53748875+wang-liang0615@users.noreply.github.com>
2024-01-02 18:04:53 +0800

    i18n (#348)

--- cmdb-ui/src/router/utils.js
+++ cmdb-ui/src/router/utils.js
@@ -6,35 +6,35 @@ export const getAppAclRouter = (appName) => {
         name: `${appName}_acl`,
         component: RouteView,
         redirect: `/${appName}/acl/users`,
-        meta: { title: '权限管理', icon: 'safety-certificate', permission: [`${appName}_admin`, 'admin'] }, // permission: 'admin'
+        meta: { title: 'commonMenu.permission', icon: 'safety-certificate', permission: [`${appName}_admin`, 'admin'] }, // permission: 'admin'
         children: [
             {
                 path: `/${appName}/acl/roles`,
                 name: `${appName}_acl_roles`,
                 hideChildrenInMenu: true,
                 component: () => import('@/modules/acl/views/roles'),
-                meta: { title: '角色管理', icon: 'team', keepAlive: true }
+                meta: { title: 'commonMenu.role', icon: 'team', keepAlive: true }
             },
             {
                 path: `/${appName}/acl/resources`,
                 name: `${appName}_acl_resources`,
                 hideChildrenInMenu: true,
                 component: () => import('@/modules/acl/views/resources'),
-                meta: { title: '资源管理', icon: 'credit-card', keepAlive: false }
+                meta: { title: 'commonMenu.resource', icon: 'credit-card', keepAlive: false }
             },
             {
                 path: `/${appName}/acl/resource_types`,
                 name: `${appName}_acl_resource_types`,
                 hideChildrenInMenu: true,
                 component: () => import('@/modules/acl/views/resource_types'),
-                meta: { title: '资源类型', icon: 'file-text', keepAlive: true }
+                meta: { title: 'commonMenu.resourceType', icon: 'file-text', keepAlive: true }
             },
             {
                 path: `/${appName}/acl/trigger`,
                 name: `${appName}_acl_trigger`,
                 hideChildrenInMenu: true,
                 component: () => import('@/modules/acl/views/trigger'),
-                meta: { title: '触发器', icon: 'clock-circle', keepAlive: true }
+                meta: { title: 'commonMenu.trigger', icon: 'clock-circle', keepAlive: true }
             }
         ]
     }
