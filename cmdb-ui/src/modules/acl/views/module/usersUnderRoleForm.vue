ef156553c6396fa924fae2868a20d7f833233e5b
wang-liang0615 <dhuwl0615@163.com>
2024-01-03 16:35:01 +0800

    format(ui)

--- cmdb-ui/src/modules/acl/views/module/usersUnderRoleForm.vue
+++ cmdb-ui/src/modules/acl/views/module/usersUnderRoleForm.vue
@@ -3,7 +3,13 @@
     <a-form-item :label="$t('acl.addUser')" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
       <a-row>
         <a-col span="15">
-          <el-select v-model="selectedChildrenRole" multiple collapse-tags size="small" filterable :placeholder="$t('placeholder2')">
+          <el-select
+            v-model="selectedChildrenRole"
+            multiple
+            collapse-tags
+            size="small"
+            filterable
+            :placeholder="$t('placeholder2')">
             <el-option
               class="drop-down-render"
               v-for="role in allRoles"
@@ -21,7 +27,12 @@
     <a-card>
       <a-row :gutter="24" v-for="(record, index) in records" :key="record.id" :style="{ marginBottom: '5px' }">
         <a-col :span="20">{{ index + 1 }}、{{ record.nickname }}</a-col>
-        <a-col :span="4"><a-button type="danger" size="small" @click="handleRevokeUser(record)">{{ $t('acl.remove') }}</a-button></a-col>
+        <a-col
+          :span="4"
+        ><a-button type="danger" size="small" @click="handleRevokeUser(record)">{{
+          $t('acl.remove')
+        }}</a-button></a-col
+        >
       </a-row>
     </a-card>
   </CustomDrawer>
