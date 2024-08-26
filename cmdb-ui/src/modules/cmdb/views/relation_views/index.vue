785c63e39728947f523f256889b8d081300b6471
LH_R <song.lhlh@gmail.com>
2024-08-26 22:21:25 +0800

    feat: export remove reference attr

--- cmdb-ui/src/modules/cmdb/views/relation_views/index.vue
+++ cmdb-ui/src/modules/cmdb/views/relation_views/index.vue
@@ -1259,7 +1259,7 @@ export default {
     },
     async openBatchDownload() {
       this.$refs.batchDownload.open({
-        preferenceAttrList: this.preferenceAttrList,
+        preferenceAttrList: this.preferenceAttrList.filter((attr) => !attr?.is_reference),
         ciTypeName: this.$route.meta.name,
       })
     },
